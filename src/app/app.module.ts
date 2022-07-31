c78c578a1de9ba0aa49846dfc25bcf81f0c3d5a1
Aayush Dhungana <90859745+aayush-dhg@users.noreply.github.com>
2022-07-31 09:49:56 -0500

    inital project setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './components/home-page/home-page.component';
+import { UserInfoComponent } from './components/user-info/user-info.component';
+import { CategoryComponent } from './components/category/category.component';
+import { CustomerComponent } from './components/customer/customer.component';
+import { CustomerProductComponent } from './components/customer-product/customer-product.component';
+import { ProductComponent } from './components/product/product.component';
+import { VendorComponent } from './components/vendor/vendor.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    UserInfoComponent,
+    CategoryComponent,
+    CustomerComponent,
+    CustomerProductComponent,
+    ProductComponent,
+    VendorComponent
   ],
   imports: [
     BrowserModule,
